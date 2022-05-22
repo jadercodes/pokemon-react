d44fa8750592b7b79a682089283629796d63facc
jadercodes <97782554+jadercodes@users.noreply.github.com>
2022-05-22 15:20:42 +0700

    built form and added API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  let [pokemon, setPokemon] = useState(null);
+  let [type, setType] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+
+  let form = (
+    <div className="Form">
+      <form onSubmit={handleSubmit}>
+        <input type="search" name="pokemon" placeholder="find a pokemon" />
+        <input type="submit" value="search" />
+      </form>
     </div>
   );
-}
 
-export default App;
+  function showPokemon(response) {
+    setLoaded(true);
+    setType(response.data.types[0].type.name);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const submitPokemon = event.target.elements.pokemon.value;
+    setPokemon(submitPokemon);
+    let url = `https://pokeapi.co/api/v2/pokemon/${submitPokemon}`;
+    axios.get(url).then(showPokemon).catch(handleError);
+  }
+
+  function handleError() {
+    setLoaded(false);
+  }
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <div>Name: {pokemon}</div>
+        <div>Type: {type}</div>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
